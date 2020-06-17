d2901b3e0f55e7dc54dc4ddd2c9de732e5759e8e
oharabutyou <oharabutyou@gmail.com>
2020-06-18 02:48:19 +0900

    simplify CUI test

--- Sudoku/SudokuCUI.java
+++ Sudoku/SudokuCUI.java
@@ -1,34 +1,15 @@
 package Sudoku;
 
-import java.util.Scanner;
-
 public class SudokuCUI {
-    static Scanner stdIn = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        String filename;
-        if (args.length != 0) {
-            System.out.println("Enter import file:");
-            filename = stdIn.next();
-        } else {
-            filename = args[0];
-        }
-        SudokuBoard b = new SudokuBoard(filename);
-        System.out.println("Imported number:");
-        System.out.println(b.display());
-        // System.out.println("Please wait...\n");
-        // b.solve();
-        // System.out.println("After solving:");
-        // System.out.println(b.display());
-        if (b.finished())
-            System.out.println("This is the answer!");
-        else {
-            System.out.println("Please wait...\n");
-            b.solveByBacktrack();
-            System.out.println("After solving:");
-            System.out.println(b.display());
-            if (b.finished())
-                System.out.println("This is the answer!");
-        }
-    }
+	public static void main(String[] args) {
+		SudokuBoard b = new SudokuBoard("data/test6.txt");
+		System.out.println("Imported number:");
+		System.out.println(b.display());
+		System.out.println("Please wait...\n");
+		b.solveByBacktrack();
+		System.out.println("After solving:");
+		System.out.println(b.display());
+		if (b.finished())
+			System.out.println("This is the answer!");
+	}
 }
